62cc1ec2a747d8ec8473dd18044d8db3ef21baaf
Andrew G <glazynovand@gmail.com>
2021-04-30 03:32:06 +0300

    get nodemailer port and host from .env

--- api-service/src/services/mailer.ts
+++ api-service/src/services/mailer.ts
@@ -1,10 +1,8 @@
-// const nodemailer = require("nodemailer");
-
 import nodemailer from 'nodemailer';
 
 const transport = nodemailer.createTransport({
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: process.env.NODEMAILER_HOST || 'smtp.mailtrap.io',
+    port: Number(process.env.NODEMAILER_PORT) || 2525,
     auth: {
         user: '847808607f5c1d',
         pass: '2b60a4a0096a90'
