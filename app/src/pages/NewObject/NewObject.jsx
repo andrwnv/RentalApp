2d6944851910274bd02c49ccb6b52120022d19bb
Andrew G <glazynovand@gmail.com>
2021-05-05 14:43:10 +0300

    [ui] connect b-e creation ad with ui

--- app/src/pages/NewObject/NewObject.jsx
+++ app/src/pages/NewObject/NewObject.jsx
@@ -8,14 +8,25 @@ import airbnb from '../../assets/airbnb.png';
 import api from '../../services/api';
 
 import './NewObject.css';
+import Cookies from '../../services/cookies';
 
 
 export default function NewObject({history}) {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
-    const [city, setCity] = useState('');
     const [houseNumber, setHouseNumber] = useState('');
+    const [country, setCountries] = useState('');
+    const [street, setStreets] = useState('');
+    const [locality, setLocalities] = useState('');
+    const [localityType, setLocalitiesTypes] = useState('');
+    const [houseType, setHouseType] = useState('');
     const [price, setPrice] = useState('');
+
+    const [apiCountries, setApiCountries] = useState([]);
+    const [apiStreets, setApiStreets] = useState([]);
+    const [apiLocalities, setApiLocalities] = useState([]);
+    const [apiLocalitiesTypes, setApiLocalitiesTypes] = useState([]);
+    const [apiHouseTypes, setApiHouseType] = useState([]);
 
     const [files, setFiles] = useState(null);
     const photos = [];
@@ -40,9 +51,31 @@ export default function NewObject({history}) {
     async function handleSubmit(event) {
         event.preventDefault();
 
-        api.post();
+        const data = {
+            title: title,
+            description: description,
+            price: price,
+            country: country,
+            street: street,
+            localityType: localityType,
+            locality: locality,
+            houseNumber: houseNumber,
+            objectType: houseType
+        }
 
-        // history.push('/dashboard');
+        const token = Cookies.get('token');
+        const headers = {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            token: `${token}`
+        };
+
+        api.post('http://localhost:3080/rent_ads', data, {headers}).then(res => {
+            alert('Объявление создано!');
+            history.push('/dashboard');
+        }).catch(err => {
+            alert('Не корректно заполнены поля!');
+        });
     }
 
     const aquaticCreatures = [
@@ -58,6 +91,85 @@ export default function NewObject({history}) {
         {label: 'Предметы первой необходимости', value: 'essentials'},
         {label: 'Вечеринки', value: 'party'},
     ];
+
+    if( apiCountries.length === 0 ) {
+        api.get('http://localhost:3080/classifiers/countries').then((res) => {
+            const data = res.data.data;
+            const _apiCountries = [];
+
+            data.forEach(item => {
+                // console.log(item.name);
+                _apiCountries.push({
+                    label: item.name,
+                    value: item.name
+                });
+            });
+
+            setApiCountries(_apiCountries);
+        });
+    }
+
+    if( apiStreets.length === 0 ) {
+        api.get('http://localhost:3080/classifiers/streets').then((res) => {
+            const data = res.data.data;
+            const _apiStreets = [];
+
+            data.forEach(item => {
+                _apiStreets.push({
+                    label: item.name,
+                    value: item.name
+                });
+            });
+
+            setApiStreets(_apiStreets);
+        });
+    }
+
+    if( apiLocalities.length === 0 ) {
+        api.get('http://localhost:3080/classifiers/localities').then((res) => {
+            const data = res.data.data;
+            const _apiLocalities = [];
+
+            data.forEach(item => {
+                _apiLocalities.push({
+                    label: item.name,
+                    value: item.name
+                });
+            });
+
+            setApiLocalities(_apiLocalities);
+        });
+    }
+
+    if( apiLocalitiesTypes.length === 0 ) {
+        api.get('http://localhost:3080/classifiers/localities_types').then((res) => {
+            const data = res.data.data;
+            const _apiLocalitiesTypes = [];
+            data.forEach(item => {
+                _apiLocalitiesTypes.push({
+                    label: item.name,
+                    value: item.name
+                });
+            });
+            setApiLocalitiesTypes(_apiLocalitiesTypes);
+        });
+    }
+
+    if( apiHouseTypes.length === 0 ) {
+        api.get('http://localhost:3080/classifiers/house_types').then((res) => {
+            const data = res.data.data;
+            const _apiHouseTypes = [];
+
+            data.forEach(item => {
+                _apiHouseTypes.push({
+                    label: item.typeName,
+                    value: item.typeName
+                });
+            });
+
+            setApiHouseType(_apiHouseTypes);
+        });
+    }
 
     return (
         <div className = "background">
@@ -84,9 +196,11 @@ export default function NewObject({history}) {
                             <img src = {camera} alt = "Select img" />
                         </label>
 
-                        <Carousel style = {{
-                            marginBottom: '8px'
-                        }}>
+                        <Carousel
+                            style = {{
+                                marginBottom: '8px'
+                            }}
+                        >
                             {photos}
                         </Carousel>
 
@@ -101,24 +215,65 @@ export default function NewObject({history}) {
 
                         <label htmlFor = "description">Описание собственности *</label>
                         <textarea
-                            id="description"
-                            value={description}
+                            id = "description"
+                            value = {description}
                             placeholder = "Описание вашей собственности..."
-                            onChange={event => setDescription(event.target.value)}
-                            cols="40"
-                            rows="5"
+                            onChange = {event => setDescription(event.target.value)}
+                            cols = "40"
+                            rows = "5"
                         />
 
-                        <label htmlFor = "city">Адрес *</label>
-                        <input
-                            type = "text"
-                            id = "city"
-                            value = {city}
-                            placeholder = "Название города..."
-                            onChange = {event => setCity(event.target.value)}
+                        <label htmlFor = "objectType">Тип объекта *</label>
+                        <Select
+                            options = {apiHouseTypes}
+                            id = 'objectType'
+                            placeholder = {'Название типа объекта...'}
+                            onChange = {event => {
+                                setHouseType(event.value);
+                            }}
                         />
 
-                        <label htmlFor = "houseNumber">Номер дома *</label>
+                        <label htmlFor = "country" className = 'select'>Страна *</label>
+                        <Select
+                            options = {apiCountries}
+                            id = 'country'
+                            placeholder = {'Название страны...'}
+                            onChange = {event => {
+                                setCountries(event.value);
+                            }}
+                        />
+
+                        <label htmlFor = "streets" className = 'select'>Улица *</label>
+                        <Select
+                            options = {apiStreets}
+                            id = 'streets'
+                            placeholder = {'Название улицы...'}
+                            onChange = {event => {
+                                setStreets(event.value);
+                            }}
+                        />
+
+                        <label htmlFor = "localityType" className = 'select'>Тип населенного пункта *</label>
+                        <Select
+                            options = {apiLocalitiesTypes}
+                            id = 'localityType'
+                            placeholder = {'Название типа населенного пункта...'}
+                            onChange = {event => {
+                                setLocalitiesTypes(event.value);
+                            }}
+                        />
+
+                        <label htmlFor = "locality" className = 'select'>Населенный пункт *</label>
+                        <Select
+                            options = {apiLocalities}
+                            id = 'locality'
+                            placeholder = {'Название населенного пункта...'}
+                            onChange = {event => {
+                                setLocalities(event.value);
+                            }}
+                        />
+
+                        <label htmlFor = "houseNumber" className = 'select'>Номер дома *</label>
                         <input
                             type = "text"
                             id = "houseNumber"
@@ -133,6 +288,10 @@ export default function NewObject({history}) {
                             id = 'items'
                             placeholder = {'Удобства...'}
                             isMulti
+                            onChange = {event => {
+                                console.log(event);
+                                console.log('Im here');
+                            }}
                         />
 
                         <label htmlFor = "price" className = "margin">Стоимость (Руб/день) *</label>
