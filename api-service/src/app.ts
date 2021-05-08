9a07bf21090a40ff513389b0e177a45415cb1892
Andrew G <glazynovand@gmail.com>
2021-05-08 15:25:16 +0300

    add adminBro admin panel

--- api-service/src/app.ts
+++ api-service/src/app.ts
@@ -2,6 +2,12 @@ import routes from './routes';
 import dotenv from 'dotenv';
 import passport from 'passport';
 
+import AdminBro from 'admin-bro';
+import AdminBroExpress from '@admin-bro/express';
+import AdminBroSequelize from '@admin-bro/sequelize';
+
+import Connection from './models/db_models';
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const http = require('http');
@@ -11,6 +17,35 @@ dotenv.config();
 // Init express.
 const server = express();
 const port = process.env.PORT || 3080;
+
+AdminBro.registerAdapter(AdminBroSequelize);
+
+const adminBro = new AdminBro({
+    databases: [Connection],
+    rootPath: '/admin',
+    branding: {
+        logo: 'https://res.cloudinary.com/rentalappclone/image/upload/c_scale,w_100/v1620461545/logomain.png',
+        companyName: 'Rental App'
+    }
+})
+
+const ADMIN_USER = {
+    eMail: process.env.ADMIN_EMAIL || 'username',
+    password: process.env.ADMIN_PASS || 'username'
+}
+
+server.use(adminBro.options.rootPath, AdminBroExpress.buildAuthenticatedRouter(adminBro, {
+    cookieName: process.env.ADMIN_EMAIL || 'admin@rental_app.gg',
+    cookiePassword: process.env.ADMIN_PASS || 'username',
+    authenticate: async (username, password) => {
+        if (username === ADMIN_USER.eMail && password === ADMIN_USER.password) {
+            return ADMIN_USER;
+        }
+
+        return null;
+    }
+}));
+
 
 server.use(bodyParser.urlencoded({
     extended: true,
