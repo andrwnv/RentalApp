afc9b56c0b8f19f36b03298270da5e6176c2ba2d
Andrew G <glazynovand@gmail.com>
2021-05-23 18:09:51 +0700

    booking api done

--- api-service/src/api/booking_controller.ts
+++ api-service/src/api/booking_controller.ts
@@ -1,11 +1,66 @@
 import express from 'express';
 
 import Connection from '../models/db_models';
+import Client from '../models/types/client_type';
+
+const isNotLandlord = async (client: Client, objectId: number): Promise<boolean | undefined> => {
+    const object = await Connection.models.object.findOne({
+        where: {
+            id: objectId
+        }
+    });
+
+    if (object === null) {
+        return undefined;
+    }
+
+    return object.get('FK_landLord') !== client.id;
+}
 
 class BookingController {
     async index(req: express.Request, res: express.Response) {
         try {
+            const reservation = await Connection.models.bookedObject.findOne({
+                where: {
+                    id: req.params.id
+                }
+            });
 
+            res.status(200).json({
+                status: 'Success',
+                data: reservation
+            });
+        } catch(err) {
+            res.status(500).json({
+                status: 'Error',
+                data: err
+            });
+        }
+    }
+
+    async userReservations(req: express.Request, res: express.Response) {
+        try {
+            const client = req.user as Client;
+
+            if (client === null) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'No user data!'
+                });
+
+                return;
+            }
+
+            const reservations = await Connection.models.bookedObject.findAll({
+                where: {
+                    FK_client: client.id
+                }
+            });
+
+            res.status(200).json({
+                status: 'Success',
+                data: reservations
+            });
         } catch(err) {
             res.status(500).json({
                 status: 'Error',
@@ -16,7 +71,52 @@ class BookingController {
 
     async create(req: express.Request, res: express.Response) {
         try {
+            const client = req.user as Client;
 
+            if (client === null) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'No user data!'
+                });
+
+                return;
+            }
+
+            if ( new Date(req.body.beginDate) < new Date() ||
+                 new Date(req.body.endDate) <= new Date() ||
+                 new Date(req.body.endDate) <= new Date(req.body.beginDate)) {
+
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'Incorrect dates!'
+                });
+
+                return;
+            }
+
+            if (! await isNotLandlord(client, req.body.objectId)) {
+                res.status(403).json({
+                    status: 'Error',
+                    data: 'Landlord cant booking his object!'
+                });
+
+                return;
+            }
+
+            const bookingData = {
+                beginDate: req.body.beginDate,
+                endDate: req.body.endDate,
+                FK_client: client.id,
+                FK_object: req.body.objectId,
+                confirmed: false
+            };
+
+            const reservation = await Connection.models.bookedObject.create(bookingData);
+
+            res.status(200).json({
+                status: 'Success',
+                data: reservation
+            });
         } catch(err) {
             res.status(500).json({
                 status: 'Error',
@@ -27,7 +127,54 @@ class BookingController {
 
     async delete(req: express.Request, res: express.Response) {
         try {
+            const landLord = req.user as Client;
 
+            if (landLord === null) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'No user data!'
+                });
+
+                return;
+            }
+
+            const reservation = await Connection.models.bookedObject.findOne({
+                where: {
+                    id: req.body.bookingId
+                }
+            });
+
+            if (reservation === null) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'That reservation not found!'
+                });
+
+                return;
+            }
+
+            const isLandLordObject = await Connection.models.object.findOne({
+                where: {
+                    id: reservation.get('FK_object') as number,
+                    FK_landLord: landLord.id
+                }
+            });
+
+            if (isLandLordObject === null) {
+                res.status(403).json({
+                    status: 'Error',
+                    data: 'Only land lord can delete reservation!'
+                });
+
+                return;
+            }
+
+            await reservation.destroy();
+
+            res.status(200).json({
+                status: 'Success',
+                data: 'Reservation deleted!'
+            });
         } catch(err) {
             res.status(500).json({
                 status: 'Error',
@@ -38,7 +185,56 @@ class BookingController {
 
     async confirm(req: express.Request, res: express.Response) {
         try {
+            const landLord = req.user as Client;
 
+            if (landLord === null) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'No user data!'
+                });
+
+                return;
+            }
+
+            const reservation = await Connection.models.bookedObject.findOne({
+                where: {
+                    id: req.body.bookingId
+                }
+            });
+
+            if (reservation === null) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'That reservation not found!'
+                });
+
+                return;
+            }
+
+            const isLandLordObject = await Connection.models.object.findOne({
+                where: {
+                    id: reservation.get('FK_object') as number,
+                    FK_landLord: landLord.id
+                }
+            });
+
+            if (isLandLordObject === null) {
+                res.status(403).json({
+                    status: 'Error',
+                    data: 'Confirm by no land lord!'
+                });
+
+                return;
+            }
+
+            await reservation.update({
+               confirmed: true
+            });
+
+            res.status(200).json({
+                status: 'Success',
+                data: 'Reservation confirmed!'
+            });
         } catch(err) {
             res.status(500).json({
                 status: 'Error',
--- api-service/src/routers/booking.router.ts
+++ api-service/src/routers/booking.router.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 
+import passport from '../services/passport';
 import { BookingCtrl } from '../api/booking_controller';
 
 const bookingRouter = express.Router();
 
-bookingRouter.patch('/confirm', BookingCtrl.confirm);
-bookingRouter.delete('/', BookingCtrl.delete);
-bookingRouter.post('/', BookingCtrl.create);
-bookingRouter.get('/:id', BookingCtrl.index);
+bookingRouter.patch('/confirm', passport.authenticate('jwt', { session: false }), BookingCtrl.confirm);
+bookingRouter.get('/', passport.authenticate('jwt', { session: false }), BookingCtrl.userReservations);
+bookingRouter.delete('/', passport.authenticate('jwt', { session: false }), BookingCtrl.delete);
+bookingRouter.post('/', passport.authenticate('jwt', { session: false }), BookingCtrl.create);
+bookingRouter.get('/:id', passport.authenticate('jwt', { session: false }), BookingCtrl.index);
 
 export default bookingRouter;
