44433539e19bf9773215c73e4775b23cb49ae335
Andrew G <glazynovand@gmail.com>
2021-05-24 21:04:26 +0700

    add rent controller methods

--- api-service/src/api/rent_controller.ts
+++ api-service/src/api/rent_controller.ts
@@ -3,11 +3,70 @@ import express from 'express';
 import Connection from '../models/db_models';
 import Client from '../models/types/client_type';
 
+
+const calcTotalPrice = (beginDate: Date, endDate: Date, oneDayPrice: number, products: any[]) => {
+    let totalSum = oneDayPrice;
+
+    products.forEach(val => {
+        totalSum += parseInt(val.get('price'));
+    });
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const dayCount = Math.round(Math.abs((beginDate.getTime() - endDate.getTime()) / oneDay)) + 1;
+
+    return totalSum * dayCount;
+}
+
 class RentController {
     async index(req: express.Request, res: express.Response) {
         try {
-            const client = req.user as Client;
+            const rentData = await Connection.models.rentedObject.findOne({
+                where: {
+                    id: req.params.id
+                }
+            });
 
+            if ( !rentData ) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'Cant find rend data'
+                });
+
+                return;
+            }
+
+            res.status(200).json({
+                status: 'Success',
+                data: rentData
+            });
+        } catch(err) {
+            res.status(500).json({
+                status: 'Error',
+                data: err
+            });
+        }
+    }
+
+    async delete(req: express.Request, res: express.Response) {
+        try {
+            const rentData = await Connection.models.rentedObject.findOne({
+                where: {
+                    id: req.body.rentId
+                }
+            });
+
+            if ( !rentData ) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'Cant find rend data!'
+                });
+
+                return;
+            }
+
+            await rentData.destroy();
+
+            res.status(200).json({ status: 'Success' });
         } catch(err) {
             res.status(500).json({
                 status: 'Error',
@@ -18,7 +77,55 @@ class RentController {
 
     async moveFromBooking(req: express.Request, res: express.Response) {
         try {
-            const landlord = req.user as Client;
+            const reservation = await Connection.models.bookedObject.findOne({
+                where: {
+                    id: req.body.bookingId
+                },
+            });
+
+            if ( !reservation ) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'Bad request!'
+                });
+
+                return;
+            }
+
+            const object = await Connection.models.object.findOne({
+                where: {
+                    id: reservation.get('FK_object') as number
+                }
+            });
+
+            if ( !object ) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'Cant find object!'
+                });
+
+                return;
+            }
+
+            const additionalComfortProps = await Connection.models.additionalComfort.findAll({
+                where: {
+                    FK_bookingObject: req.body.bookingId as number
+                }
+            });
+
+            const rentData = await Connection.models.rentedObject.create({
+                beginDate: reservation.get('beginDate') as string,
+                endDate: reservation.get('endDate') as string,
+                price: calcTotalPrice(new Date(reservation.get('beginDate') as string), new Date(reservation.get('endDate') as string),
+                    object.get('price') as number, additionalComfortProps),
+                FK_client: reservation.get('FK_client'),
+                FK_object: object.get('id')
+            });
+
+            res.status(200).json({
+                status: 'Success',
+                data: rentData
+            });
 
         } catch(err) {
             res.status(500).json({
@@ -30,8 +137,32 @@ class RentController {
 
     async moveToHistory(req: express.Request, res: express.Response) {
         try {
-            const landlord = req.user as Client;
+            const rentData = await Connection.models.rentedObject.findOne({
+                where: {
+                    id: req.body.rentId
+                }
+            });
 
+            if ( !rentData ) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'Cant find rend data'
+                });
+
+                return;
+            }
+
+            const history = await Connection.models.userRentalHistory.create({
+                beginDate: rentData.get('beginDate'),
+                endDate: rentData.get('endDate'),
+                FK_object: rentData.get('FK_object'),
+                FK_client: rentData.get('FK_client')
+            });
+
+            res.status(200).json({
+                status: 'Success',
+                data: history
+            });
         } catch(err) {
             res.status(500).json({
                 status: 'Error',
@@ -42,8 +173,25 @@ class RentController {
 
     async userHistory(req: express.Request, res: express.Response) {
         try {
-            const landlord = req.user as Client;
+            const client = req.user as Client;
 
+            if ( !client ) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'Cant find user data'
+                });
+            }
+
+            const rentData = await Connection.models.rentedObject.findAll({
+                where: {
+                    FK_client: client.id
+                }
+            });
+
+            res.status(200).json({
+                status: 'Success',
+                data: rentData
+            });
         } catch(err) {
             res.status(500).json({
                 status: 'Error',
--- api-service/src/routers/rent.router.ts
+++ api-service/src/routers/rent.router.ts
@@ -5,7 +5,8 @@ import { RentCtrl } from '../api/rent_controller';
 
 const rentRouter = express.Router();
 
-rentRouter.delete('/move_to_history', passport.authenticate('jwt', { session: false }), RentCtrl.moveToHistory);
+rentRouter.post('/move_to_history', passport.authenticate('jwt', { session: false }), RentCtrl.moveToHistory);
+rentRouter.delete('/', passport.authenticate('jwt', { session: false }), RentCtrl.delete);
 rentRouter.post('/', passport.authenticate('jwt', { session: false }), RentCtrl.moveFromBooking);
 rentRouter.get('/', passport.authenticate('jwt', { session: false }), RentCtrl.userHistory);
 rentRouter.get('/:id', passport.authenticate('jwt', { session: false }), RentCtrl.index);
