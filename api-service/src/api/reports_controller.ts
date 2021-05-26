c6a1e2cf53d7fa3ca70e73bb27f2e97b8b60a9e3
Andrew G <glazynovand@gmail.com>
2021-05-26 22:30:04 +0700

    add report api logic

--- api-service/src/api/reports_controller.ts
+++ api-service/src/api/reports_controller.ts
@@ -1,13 +1,53 @@
 import express from 'express';
 
 import Connection from '../models/db_models';
+import Client from '../models/types/client_type';
+
 
 class ReportsController {
     async create(req: express.Request, res: express.Response) {
         try {
+            if (req.body.objectReviewId && req.body.clientReviewId) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'Bad request!'
+                });
+            }
+
+            const isReasonExist = await Connection.models.reportReasons.findOne({
+                where: {
+                    id: req.body.reasonId
+                }
+            })
+
+            if (!isReasonExist) {
+                res.status(400).json({
+                    status: 'Error',
+                    data: 'Incorrect reason!'
+                });
+            }
+
+            let report = undefined;
+
+            const client: Client = req.user as Client;
+
+            if (req.body.objectReviewId) {
+                report = await Connection.models.reports.create({
+                    FK_client: client.id,
+                    FK_reason: req.body.reasonId,
+                    FK_objectReview: req.body.objectReviewId
+                });
+            } else if (req.body.clientReviewId) {
+                report = await Connection.models.reports.create({
+                    FK_client: client.id,
+                    FK_reason: req.body.reasonId,
+                    FK_clientReview: req.body.clientReviewId
+                });
+            }
+
             res.status(200).json({
                 status: 'Success',
-                data: ''
+                data: report
             });
         } catch(err) {
             res.status(500).json({
@@ -19,9 +59,13 @@ class ReportsController {
 
     async reasons(req: express.Request, res: express.Response) {
         try {
+            const reasons = await Connection.models.reportReasons.findAll({
+                attributes: ['id', 'name', 'description']
+            });
+
             res.status(200).json({
                 status: 'Success',
-                data: ''
+                data: reasons
             });
         } catch(err) {
             res.status(500).json({
