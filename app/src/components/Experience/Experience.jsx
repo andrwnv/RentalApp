00e5b032e3fb5dbf7245224c8fe015476cea8e51
Andrew G <glazynovand@gmail.com>
2021-05-29 22:19:09 +0700

    [ui]: load filtered data. PROJECT FINISHED (I HOPE)

--- app/src/components/Experience/Experience.jsx
+++ app/src/components/Experience/Experience.jsx
@@ -1,110 +1,21 @@
-import { Col, Button, Form, FormControl, Row, Container } from 'react-bootstrap';
 import React from 'react';
 
 import './Experience.css';
-import Slider from '@material-ui/core/Slider';
-
 
 export default class Experience extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            priceValue: [0, 85],
-            ratingValue: [0, 10],
-            maxRatingValue: 10,
-            maxPriceValue: 100,
-            beginDate: '',
-            endDate: '',
         }
 
         this.fullName = `${localStorage.getItem('firstName')}`;
-    }
-
-    search = () => {
-
     }
 
     render() {
         return (
             <div>
                 <h2 className = "User">{this.fullName}, что ты хочешь найти?</h2>
-
-                <Container>
-                    <Row
-                        className = 'justify-content-md-center' style = {{marginBottom: '20px', marginTop: '20px'}}
-                    >
-                        <p>Выберите пределы стоиомсти (руб/день)</p>
-                        <Slider
-                            value = {this.state.priceValue}
-                            step = {5}
-                            max = {this.state.maxPriceValue}
-                            onChange = {(event, value) => {
-                                this.setState({priceValue: value, maxPriceValue: value[1] + 1500});
-                            }}
-                            valueLabelDisplay = 'auto'
-                            aria-labelledby = 'range-slider'
-                        />
-                    </Row>
-
-                    <Row
-                        className = 'justify-content-md-center' style = {{marginBottom: '20px', marginTop: '20px'}}
-                    >
-                        <p>Выберите пределы рейтинга</p>
-                        <Slider
-                            value = {this.state.ratingValue}
-                            step = {1}
-                            max = {this.state.maxRatingValue}
-                            onChange = {(event, value) => {
-                                this.setState({ratingValue: value});
-                            }}
-                            valueLabelDisplay = 'auto'
-                            aria-labelledby = 'range-slider'
-                        />
-                    </Row>
-
-                    <Row className = 'justify-content-md-center' style = {{marginBottom: '2em'}}>
-                        <Col>
-                           <p style = {{textAlign: 'center'}}>Дата начала</p>
-                            <Form.Control
-                                onChange = {(event) => {
-                                    this.setState({beginDate: event.target.value});
-                                }}
-                                value = {this.state.beginDate}
-                                type = 'date' name = 'beginDate'
-                                style = {{
-                                    width: '50%',
-                                    marginTop: 0,
-                                    marginRight: 'auto',
-                                    marginBottom: 0,
-                                    marginLeft: 'auto',
-                                }}
-                            />
-                        </Col>
-
-                        <Col>
-                           <p style = {{textAlign: 'center'}}>Дата окончания</p>
-                            <Form.Control
-                                onChange = {(event) => {
-                                    this.setState({endDate: event.target.value});
-                                }}
-                                value = {this.state.endDate}
-                                type = 'date' name = 'beginDate'
-                                style = {{
-                                    width: '50%',
-                                    marginTop: 0,
-                                    marginRight: 'auto',
-                                    marginBottom: 0,
-                                    marginLeft: 'auto',
-                                }}
-                            />
-                        </Col>
-                    </Row>
-
-                    <Row className = 'justify-content-md-center'>
-                        <Button style = {{width: '40%'}} onClick = {this.search}>Поиск</Button>
-                    </Row>
-                </Container>
             </div>
         );
     }
--- app/src/pages/Dashboard/Dashboard.jsx
+++ app/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import Experience from '../../components/Experience/Experience';
-import { Container, Button, Row } from 'react-bootstrap';
+import { Container, Button, Row, Col, Form } from 'react-bootstrap';
 
 import Header from '../../components/Header/Header';
 import api from '../../services/api';
@@ -10,26 +10,38 @@ import './Dashboard.css';
 import { Ads } from '../../components/Ads/Ads';
 
 import Cookies from '../../services/cookies';
+import Slider from '@material-ui/core/Slider';
 
 export default function Dashboard() {
+    const maxRatingValue = 10;
+
     const [data, setData] = useState(undefined);
     const [isEndData, setIsEndData] = useState(false);
     const [buttonText, setButtonText] = useState('Загрузить еще...');
+    const [priceValue, setPriceValue] = useState([0, 85]);
+    const [ratingValue, setRatingValue] = useState([0, 10]);
+    const [maxPriceValue, setMaxPriceValue] = useState(100);
+    const [beginDate, setBeginDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [filtered, setFiltered] = useState(null);
+    const [usedFilter, setUsedFilter] = useState(false);
 
     localStorage.setItem('countForLoad', '' + 4);
     localStorage.setItem('currentPaddingForLoad', '' + 0);
 
+    const headers = {
+        headers: {
+            'token': Cookies.get('token'),
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    };
+
     const getAdsData = (count, padding) => {
-        api.get(`http://localhost:3080/rent_ads/all?count=${count}&padding=${padding}`, {
-            headers: {
-                'token': Cookies.get('token'),
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        }).then(res => {
+        api.get(`http://localhost:3080/rent_ads/all?count=${count}&padding=${padding}`, headers).then(res => {
             const loadedData = res.data.data;
 
-            if (loadedData.length === 0) {
+            if( loadedData.length === 0 ) {
                 setIsEndData(true);
                 setButtonText('Больше нет...');
             } else {
@@ -46,7 +58,6 @@ export default function Dashboard() {
     if( data === undefined ) {
         getAdsData(localStorage.getItem('countForLoad'),
             localStorage.getItem('currentPaddingForLoad'));
-
     } else {
         let curIndex = 0;
         data.forEach(obj => {
@@ -57,17 +68,68 @@ export default function Dashboard() {
                 curIndex++;
             }
 
-            jsxData[curIndex].push(<Ads name = {obj.title} desc = {obj.description}
-                                        picLinks = {obj.mediaLinks.urls[0]} objectId = {obj.id}/>);
+            jsxData[curIndex].push(<Ads
+                name = {obj.title} desc = {obj.description}
+                picLinks = {obj.mediaLinks.urls[0]} objectId = {obj.id}
+            />);
         });
     }
+
+    const search = () => {
+        const _beginDate = new Date(beginDate);
+        const _endDate = new Date(endDate);
+        const today = new Date();
+
+        if( _beginDate < today || _endDate < today || !beginDate || !endDate) {
+            alert('Заполните все поля дат!');
+            return;
+        }
+
+        console.log(JSON.stringify(priceValue));
+        console.log(JSON.stringify(ratingValue));
+        console.log(JSON.stringify([new Date(beginDate).getTime(), new Date(endDate).getTime()]));
+
+        api.get(`http://localhost:3080/rent_ads/filter?date=${JSON.stringify([new Date(beginDate).getTime(), new Date(endDate).getTime()])}&rating=${JSON.stringify(ratingValue)}&price=${JSON.stringify(priceValue)}`, headers)
+            .then(res => {
+                let data = [];
+                let blocks = [];
+
+                res.data.data.forEach(obj => {
+                    data.push(
+                        <Ads
+                            name = {obj.title} desc = {obj.description}
+                            picLinks = {obj.mediaLinks.urls[0]} objectId = {obj.id}
+                        />
+                    );
+
+                    if (data.length % 4 === 0 && data.length !== 0) {
+                        blocks.push(
+                            <Row
+                                className = 'row justify-content-center' style = {{
+                                flexWrap: 'nowrap',
+                            }}
+                            >
+                                {data}
+                            </Row>
+                        );
+
+                        data = [];
+                    }
+                });
+
+                setUsedFilter(true);
+                setFiltered(blocks);
+            });
+    };
 
     const resJSX = [];
     jsxData.forEach(arr => {
         resJSX.push(
-            <Row className = "row justify-content-center" style = {{
+            <Row
+                className = 'row justify-content-center' style = {{
                 flexWrap: 'nowrap',
-            }}>
+            }}
+            >
                 {arr}
             </Row>
         );
@@ -81,23 +143,123 @@ export default function Dashboard() {
     };
 
     return (
-        <div className = "containerDashboard">
+        <div className = 'containerDashboard'>
             <Header />
 
             <Experience />
-            <h2 className = "headText">Рекомендуется для вас</h2>
-            <div className = "dataContainer">
-                {resJSX}
-            </div>
-
-            <Container style = {{width: '15%', marginBottom: '20px', marginTop: '10px'}}>
-                <Button variant = "outline-primary"
-                        onClick = {loadMoreEvent}
-                        disabled={isEndData}
+            <Container>
+                <Row
+                    className = 'justify-content-md-center' style = {{marginBottom: '20px', marginTop: '20px'}}
                 >
-                    {buttonText}
-                </Button>
+                    <p>Выберите пределы стоиомсти (руб/день)</p>
+                    <Slider
+                        value = {priceValue}
+                        step = {5}
+                        max = {maxPriceValue}
+                        onChange = {(event, value) => {
+                            setPriceValue(value);
+                            setMaxPriceValue(value[1] + 1500);
+                        }}
+                        valueLabelDisplay = 'auto'
+                        aria-labelledby = 'range-slider'
+                    />
+                </Row>
+
+                <Row
+                    className = 'justify-content-md-center' style = {{marginBottom: '20px', marginTop: '20px'}}
+                >
+                    <p>Выберите пределы рейтинга</p>
+                    <Slider
+                        value = {ratingValue}
+                        step = {1}
+                        max = {maxRatingValue}
+                        onChange = {(event, value) => {
+                            setRatingValue(value);
+                        }}
+                        valueLabelDisplay = 'auto'
+                        aria-labelledby = 'range-slider'
+                    />
+                </Row>
+
+                <Row className = 'justify-content-md-center' style = {{marginBottom: '2em'}}>
+                    <Col>
+                       <p style = {{textAlign: 'center'}}>Дата начала</p>
+                        <Form.Control
+                            onChange = {(event) => {
+                                setBeginDate(event.target.value);
+                            }}
+                            value = {beginDate}
+                            type = 'date' name = 'beginDate'
+                            style = {{
+                                width: '50%',
+                                marginTop: 0,
+                                marginRight: 'auto',
+                                marginBottom: 0,
+                                marginLeft: 'auto',
+                            }}
+                        />
+                    </Col>
+
+                    <Col>
+                       <p style = {{textAlign: 'center'}}>Дата окончания</p>
+                        <Form.Control
+                            onChange = {(event) => {
+                                setEndDate(event.target.value);
+                            }}
+                            value = {endDate}
+                            type = 'date' name = 'beginDate'
+                            style = {{
+                                width: '50%',
+                                marginTop: 0,
+                                marginRight: 'auto',
+                                marginBottom: 0,
+                                marginLeft: 'auto',
+                            }}
+                        />
+                    </Col>
+                </Row>
+
+                <Row className = 'justify-content-md-center'>
+                    <Button style = {{width: '40%'}} onClick = {search}>Поиск</Button>
+                </Row>
+
+                <Row className = 'justify-content-md-center'>
+                    <Button
+                        variant = 'success' style = {{width: '40%', marginTop: '1em', marginBottom: '2em'}} disabled = {!usedFilter}
+                        onClick = {() => {
+                            setUsedFilter(false);
+                            setFiltered(null);
+                        }}
+                    >Убрать фильтрацию</Button>
+                </Row>
             </Container>
+
+            {
+                usedFilter === false
+                    ? (
+                        <div>
+                            <h2 className = 'headText'>Рекомендуется для вас</h2>
+                            <div className = 'dataContainer'>
+                                    {resJSX}
+                            </div>
+
+                            <Container style = {{width: '15%', marginBottom: '20px', marginTop: '10px'}}>
+                                <Button
+                                    variant = 'outline-primary'
+                                    onClick = {loadMoreEvent}
+                                    disabled = {isEndData}
+                                >
+                                    {buttonText}
+                                </Button>
+                            </Container>
+                        </div>
+                    )
+                    : (
+                        <div className = 'dataContainer'>
+                                {filtered}
+                        </div>
+                    )
+            }
         </div>
     );
 };
